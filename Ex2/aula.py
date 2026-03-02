2db4a968df5babfe576f12b207da17f42a20e69c
MariB <mariinhabrt@gmail.com>
2026-03-02 10:39:16 -0300

    .

--- Ex2/aula.py
+++ Ex2/aula.py
@@ -1,7 +1,11 @@
 import re
 
-frase = "Meu CPF é 123.456.789-10"
+frases = [
+    "Meu CPF é 123.456.789-10",
+    "Cadastrar CPF 987.654.321-00",
+    "CPFs do cliente: 111.222.333-44 e 059.362.136-09"
+]
 
-numeros = re.findall(r"\d", frase)
-
-print(numeros)
+for frase in frases:
+    cpfs = re.findall(r"\d{3}\.\d{3}\.\d{3}-\d{2}", frase)
+    print(cpfs)
